0a2b31a4847e55ff439786fe3ecb0a6fa5d116aa
Piotr Olszak <wallshoccfella@gmail.com>
2022-11-12 19:45:58 +0100

    lab5

--- oolab/src/main/java/agh/ics/oop/SimulationEngine.java
+++ oolab/src/main/java/agh/ics/oop/SimulationEngine.java
@@ -1,5 +1,7 @@
 package agh.ics.oop;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,11 +22,36 @@ public class SimulationEngine implements IEngine {
             }
         }
     }
+    public static JFrame createAndShowGui(String panel) {
+        JFrame frame = new JFrame("Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setSize(1000,1000);
+        return frame;
+    }
     @Override
     public void run() {
-        System.out.println(map);
+        JFrame gui = createAndShowGui(map.toString());
+        JTextArea area = new JTextArea();
+        area.setVisible(true);
+        area.setFont(area.getFont().deriveFont(30f));
+        area.setBounds(400,400,1000,1000);
+        gui.add(area);
+        Font font = new Font("Sans Serif", Font.ITALIC, 20);
+        area.setFont(font);
         for (int i = 0; i < moveDirectionList.length; i++){
             animalList.get(i % animalList.size()).move(moveDirectionList[i]);
+            area.setText("");
+            area.setText(map.toString());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                // handle the exception...
+                // For example consider calling Thread.currentThread().interrupt(); here.
+            }
             System.out.println(map);
         }
     }
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -3,7 +3,11 @@ package agh.ics.oop;
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
-
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.PrintStream;
+import javax.swing.*;
 
 public class World {
     public static void main (String[] args){
@@ -11,13 +15,8 @@ public class World {
         IWorldMap map = new GrassField(10);
         Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4) };
         IEngine engine = new SimulationEngine(directions, map, positions);
-        JFrame frame = new JFrame();
-        frame.setTitle("zwierzaki na polu");
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(700,700);
-        frame.getContentPane().setBackground(Color.yellow);
-        JLabel label = new JLabel();
         engine.run();
     }
 }
+
+
